904fa4322e5b52154a23f446531345d385dffc03
David Araújo <david2araujo5@gmail.com>
2023-11-10 17:31:21 +0000

    Updated

--- deploy/expose-linkerd/reverse-proxy-link.sh
+++ deploy/expose-linkerd/reverse-proxy-link.sh
@@ -2,7 +2,7 @@
 sudo apt install nginx -y
 
 # Create symlink of the NGINX configuration file
-sudo ln -s ./tfs-ctrl/expose-linkerd/expose-linkerd.conf /etc/nginx/sites-enabled/
+sudo ln -s ./tfs-ctrl/expose-linkerd/expose-linkerd /etc/nginx/sites-enabled/
 
 # Commit the reverse proxy configurations
 sudo systemctl restart nginx
