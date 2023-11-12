b25d892b7eda3894558d7704fc3cb747d059e63b
David Araújo <david2araujo5@gmail.com>
2023-11-12 19:25:13 +0000

    Update

---
+++ deploy-tfs/reverse-proxy-linkerd.sh
@@ -0,0 +1,23 @@
+# Install NGINX
+sudo apt update && sudo apt install nginx -y
+
+echo 'server {
+    listen 8084;
+
+    location / {
+        proxy_pass http://127.0.0.1:50750;
+        proxy_set_header Host localhost;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto $scheme;
+    }
+}' > ./tfs-ctrl/expose-linkerd/expose-linkerd
+
+# Create symlink of the NGINX configuration file
+sudo ln -s ./tfs-ctrl/expose-linkerd/expose-linkerd /etc/nginx/sites-enabled/
+
+# Commit the reverse proxy configurations
+sudo systemctl restart nginx
+
+# Initiate linkerd dashboard
+linkerd viz dashboard &
